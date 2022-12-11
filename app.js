752e1f47ab061f478253b8597a6e0f537e4499fe
Manjish <manjish77@gmail.com>
2022-12-11 18:42:30 +0545

    classes and function hoisting concepts

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// const testCar = new Car(4, 'V6', 'Grey')
+
+// console.log(testCar)
+// console.log(testCar.carStats())
+
+//Since classes are not hoisted, the code above will give a runtime error.
+//Cannot access Class before initialization.
+
+//Since functions are hoisted, they can be declared anywhere and JS will hoist them to the top.
+//This enables function call before the function declaration section.
+greet('Top')
 class Car {
   constructor(doors, engine, color) {
     this.doors = doors
@@ -14,3 +25,9 @@ const cx5 = new Car(4, 'V6', 'Grey')
 
 console.log(cx5)
 console.log(cx5.carStats())
+
+function greet(location) {
+  console.log(`Hello from ${location}`)
+}
+
+greet('Bottom')
